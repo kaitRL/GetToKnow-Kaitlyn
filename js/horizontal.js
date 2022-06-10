9ce23acdb93771715668aa7cd2a5f1614a263425
kaitRL <103606636+kaitRL@users.noreply.github.com>
2022-06-10 06:46:06 -0400

    try timeline 11

--- js/horizontal.js
+++ js/horizontal.js
@@ -16,16 +16,16 @@ const init = () => {
     }
   });
 
-  let sections = gsap.utils.toArray(".left-panel");
+  let sectionsLeft = gsap.utils.toArray(".left-panel");
 
-  gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
+  gsap.to(sectionsLeft, {
+    xPercent: -100 * (sectionsLeft.length - 1),
     ease: "none",
     scrollTrigger: {
       trigger: ".left-container",
       pin: true,
       scrub: 1,
-      snap: 1 / (sections.length - 1),
+      snap: 1 / (sectionsLeft.length - 1),
       // base vertical scrolling on how wide the container is so it feels more natural.
       end: "+=3500",
     }
