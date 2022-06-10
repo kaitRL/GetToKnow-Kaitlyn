100fc31316fd7d185f44ffdd9150d1abad9e608f
kaitRL <103606636+kaitRL@users.noreply.github.com>
2022-06-10 07:06:03 -0400

    try timeline 14

--- js/horizontal.js
+++ js/horizontal.js
@@ -16,20 +16,35 @@ const init = () => {
     }
   });
 
-  let sectionsLeft = gsap.utils.toArray(".left-panel");
+  // img reveal
+  let revealContainers = document.querySelectorAll(".reveal");
 
-  gsap.to(sectionsLeft, {
-    xPercent: -100 * (sectionsLeft.length - 1),
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".left-container",
-      pin: true,
-      scrub: 1,
-      snap: 1 / (sectionsLeft.length - 1),
-      // base vertical scrolling on how wide the container is so it feels more natural.
-      end: "+=3500",
-    }
+  revealContainers.forEach((container, i) => {
+    let image = container.querySelector("img");
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: container,
+        containerAnimation: scrollTween,
+        start: "center center",
+        toggleActions: "restart none none reset"
+      }
+    });
+
+    tl.set(container, { autoAlpha: 1 });
+    tl.from(container, 1.5, {
+      xPercent: -100,
+      ease: "power2"
+    });
+    tl.from(image, 1.5, {
+      xPercent: 100,
+      scale: 1.3,
+      delay: -1.5,
+      ease: "power2"
+    });
   });
+
+
 };
 
 initGsap = init();
