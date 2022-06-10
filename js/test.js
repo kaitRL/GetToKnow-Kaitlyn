f6ae7017cd4dd201b6c253eba81bcc001e3b51bf
kaitRL <103606636+kaitRL@users.noreply.github.com>
2022-06-10 02:29:25 -0400

    try codepen stuff 17

--- js/test.js
+++ js/test.js
@@ -1,21 +1,21 @@
-const { gsap } = require("gsap/dist/gsap");
-const { ScrollTrigger } = require("gsap/dist/ScrollTrigger");
-const { ScrollToPlugin } = require("gsap/dist/ScrollToPlugin");
-const { Draggable } = require("gsap/dist/Draggable");
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable);
+const init = () => {
+  let sections = gsap.utils.toArray(".panel");
 
-let sections = gsap.utils.toArray(".panel");
+  gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".container",
+      pin: true,
+      scrub: 1,
+      snap: 1 / (sections.length - 1),
+      // base vertical scrolling on how wide the container is so it feels more natural.
+      end: "+=3500",
+    }
+  });
+};
 
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".container",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: "+=3500",
-  }
-});
+initGsap = init();
+window.onload = initGsap;
