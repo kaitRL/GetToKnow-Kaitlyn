78b60782f57df821f4fe1f3306689690d0fd50d5
kaitRL <103606636+kaitRL@users.noreply.github.com>
2022-06-10 08:57:52 -0400

    try timeline 36

--- js/arrow1.js
+++ js/arrow1.js
@@ -1,7 +1,17 @@
 var tl = new TimelineLite();
 
-tl.from(".text", 1, {drawSVG: 0})
-  .from(".dot", 1, {drawSVG: 0})
+tl.from(".line1", 1, {
+    drawSVG: 0
+  })
+  .from(".line2", 1, {
+    drawSVG: 0
+  })
+  .from(".line3", 1, {
+    drawSVG: 0
+  })
+  .from(".line4", 1, {
+    drawSVG: 0
+  })
 
 
 tl.timeScale(1);
