88af414cc15f16a914680333cbd33af2001b224e
kaitRL <103606636+kaitRL@users.noreply.github.com>
2022-06-10 07:10:06 -0400

    try timeline 16

--- js/horizontal.js
+++ js/horizontal.js
@@ -1,20 +1,20 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const init = () => {
+
   let sections = gsap.utils.toArray(".panel");
 
-  gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".container",
-      pin: true,
-      scrub: 1,
-      snap: 1 / (sections.length - 1),
-      // base vertical scrolling on how wide the container is so it feels more natural.
-      end: "+=3500",
-    }
-  });
+  let scrollTween = gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none", // <-- IMPORTANT!
+      scrollTrigger: {
+        trigger: ".container",
+        pin: true,
+        scrub: 0.1,
+        //snap: directionalSnap(1 / (sections.length - 1)),
+        end: "+=3000"
+      }
+    });
 
   // img reveal
   let revealContainers = document.querySelectorAll(".reveal");
