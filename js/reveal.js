ef68c802a72426cc944132ec150a0d0afc863eee
kaitRL <103606636+kaitRL@users.noreply.github.com>
2022-06-10 07:47:26 -0400

    try timeline 29

--- js/reveal.js
+++ js/reveal.js
@@ -1,1 +1,35 @@
+gsap.registerPlugin(ScrollTrigger);
 
+const init2 = () => {
+
+  let sectionsLeft = gsap.utils.toArray(".left-panel");
+
+  sectionsLeft.forEach(function(sectionsLeft) {
+    gsap.fromTo(sectionsLeft, {
+      scrollTrigger: {
+        trigger: section,
+        horizontal: true,
+        start: 'left left',
+        end: 'right center',
+        scrub: true,
+        markers: true,
+
+      }
+
+    })
+
+  })
+
+  var scroller = document.querySelector('html');
+
+  window.addEventListener('wheel', function(e) {
+
+    if (e.deltaY > 0) scroller.scrollLeft -= 100;
+    else scroller.scrollLeft += 100;
+
+  });
+
+};
+
+initGsap2 = init2();
+window.onload = initGsap2;
